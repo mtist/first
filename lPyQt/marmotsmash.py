dbf7a1b54d03a2706accbd8c2ccc229f13d3bf5b
mtist <hamza07132@gmail.com>
2017-07-19 12:05:24 +0300

    marmotsmash

--- lPyQt/marmotsmash.py
+++ lPyQt/marmotsmash.py
@@ -12,42 +12,104 @@ TIME = 30
 
 class MyWidget(QWidget):
 
-	def __init__(self):
-		super().__init__()
-		self.initUI()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-	def initUI(self):
-		self.now = 0
+    def initUI(self):
+        self.now = 0
 
-		self.timer = QBasicTimer()
-		self.step = 0
+        self.timer = QBasicTimer()
+        self.step = 0
 
-		grid = QGridLayout()
-		grid.setSpacing(10)
+        grid = QGridLayout()
+        grid.setSpacing(10)
 
-		self.holes = []
+        self.holes = []
 
-		x, y = 0, 0
-		for i in range(9):
-			self.holes.append(QPushButton('U', self))
-			self.holes[i].setFlat(True)
-			self.holes[i].setIcon(QIcon(UNACTIVE))
-			self.holes[i].setIconSize(QSize(200, 200))
+        x, y = 0, 0
+        for i in range(9):
+            self.holes.append(QPushButton('U', self))
+            self.holes[i].setFlat(True)
+            self.holes[i].setIcon(QIcon(UNACTIVE))
+            self.holes[i].setIconSize(QSize(200, 200))
+            self.holes[i].clicked.connect(self.smash)
 
-			grid.addWidget(self.holes[i], x, y)
-			
-			if x < 2:
-				x += 1
-			else:
-				y += 1 
-				x = 0
+            grid.addWidget(self.holes[i], x, y)
+            
+            if x < 2:
+                x += 1
+            else:
+                y += 1 
+                x = 0
 
-		self.setLayout(grid)
-		self.setWindowTitle('UI, not GUI')
-		self.setGeometry(100, 100, 720, 480)
-		self.show()
+        self.count = QLabel('0')
+        self.time = QLabel(str(TIME))
+        self.runBt = QPushButton('Start')
+        self.runBt.clicked.connect(self.startGame)
+
+        grid.addWidget(self.count, y, 0)
+        grid.addWidget(self.runBt, y, 1)
+        grid.addWidget(self.time, y, 2)
+
+
+        self.setLayout(grid)
+        self.setWindowTitle('UI, not GUI')
+        self.setGeometry(100, 100, 720, 480)
+        self.show()
+
+    def showHim(self):
+        number = randint(0, 9)
+        kind = randint(0, 1)
+        if kind:
+            self.holes[number].setText('G')
+            self.holes[number].setIcon(QIcon(GOOD))
+        else:
+            self.holes[number].setText('B')
+            self.holes[number].setIcon(QIcon(BAD))
+
+        self.now = number
+
+    def clearHim(self):
+        self.holes[self.now].setText('U')
+        self.holes[self.now].setIcon(QIcon(UNACTIVE))
+
+    def timerEvent(self, e):
+        self.clearHim()
+        self.showHim()
+
+        if self.step >= TIME * 2:
+            self.timer.stop()
+            self.runBt.setEnabled(True)
+            self.time.setText(str(TIME))
+            self.step = 0
+            self.clearHim()
+            return
+
+        self.step += 1
+
+        if self.step % 2:
+            time = int(self.time.text()) - 1
+            self.time.setText(str(time))
+    
+    def startGame(self):
+        time = TIME * 1000 // 60
+        self.timer.start(time, self)
+        self.count.setText('0')
+        self.runBt.setEnabled(False)
+
+    def smash(self):
+        sender = self.sender()
+        if sender.text() == 'B':
+            self.clearHim()
+            count = int(self.count.text()) + 1
+            self.count.setText(str(count))
+        elif sender.text() == 'G':
+            self.clearHim()
+            count = int(self.count.text()) - 1
+            self.count.setText(str(count))
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	window = MyWidget()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    window = MyWidget()
+    sys.exit(app.exec_())
